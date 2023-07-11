503d1e1d711a63f8be465dc8d9bf770577ae4c92
AMRIT KUMAR <123072668+amr2t@users.noreply.github.com>
2023-07-11 13:53:24 +0530

    backend structure added

---
+++ arg_platform/src/main/java/com/trade/arg_platform/ArgPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.trade.arg_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArgPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArgPlatformApplication.class, args);
+	}
+
+}
---
+++ arg_platform/src/test/java/com/trade/arg_platform/ArgPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trade.arg_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArgPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
