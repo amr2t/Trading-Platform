45cb5d0b452df2d4704b71b0463ea3f7a51952f8
AMRIT KUMAR <123072668+amr2t@users.noreply.github.com>
2023-07-14 11:42:44 +0530

    Entity errors resolved

--- arg_platform/src/main/java/com/trade/arg_platform/entity/portfolio.java
+++ arg_platform/src/main/java/com/trade/arg_platform/entity/portfolio.java
@@ -11,8 +11,7 @@ public class portfolio {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-    private String desc;
-
+    private String describe;
     @OneToMany(mappedBy = "portfolio")
     private List<holding> holdingList;
 
@@ -20,66 +19,5 @@ public class portfolio {
     @ManyToMany(mappedBy = "portfolioList")
     private List<user> userList;
 
-    public portfolio(Long id, String name, String desc, List<holding> holdingList, List<user> userList) {
-        this.id = id;
-        this.name = name;
-        this.desc = desc;
-        this.holdingList = holdingList;
-        this.userList = userList;
-    }
 
-    public portfolio() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDesc() {
-        return desc;
-    }
-
-    public void setDesc(String desc) {
-        this.desc = desc;
-    }
-
-    public List<holding> getHoldingList() {
-        return holdingList;
-    }
-
-    public void setHoldingList(List<holding> holdingList) {
-        this.holdingList = holdingList;
-    }
-
-    public List<user> getUserList() {
-        return userList;
-    }
-
-    public void setUserList(List<user> userList) {
-        this.userList = userList;
-    }
-
-
-    @Override
-    public String toString() {
-        return "portfolio{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", desc='" + desc + '\'' +
-                ", holdingList=" + holdingList +
-                ", userList=" + userList +
-                '}';
-    }
 }
--- arg_platform/src/main/java/com/trade/arg_platform/entity/stock.java
+++ arg_platform/src/main/java/com/trade/arg_platform/entity/stock.java
@@ -25,52 +25,5 @@ public class stock {
     public stock() {
     }
 
-    public stock(String symbol, String companyName, String sector, String industry) {
-        this.symbol = symbol;
-        this.companyName = companyName;
-        this.sector = sector;
-        this.industry = industry;
-    }
 
-    public String getSymbol() {
-        return symbol;
-    }
-
-    public void setSymbol(String symbol) {
-        this.symbol = symbol;
-    }
-
-    public String getCompanyName() {
-        return companyName;
-    }
-
-    public void setCompanyName(String companyName) {
-        this.companyName = companyName;
-    }
-
-    public String getSector() {
-        return sector;
-    }
-
-    public void setSector(String sector) {
-        this.sector = sector;
-    }
-
-    public String getIndustry() {
-        return industry;
-    }
-
-    public void setIndustry(String industry) {
-        this.industry = industry;
-    }
-
-    @Override
-    public String toString() {
-        return "stock{" +
-                "symbol='" + symbol + '\'' +
-                ", companyName='" + companyName + '\'' +
-                ", sector='" + sector + '\'' +
-                ", industry='" + industry + '\'' +
-                '}';
-    }
 }
