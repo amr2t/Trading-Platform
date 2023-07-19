825555d90302d0034206b480dfca78c54efa62ce
AMRIT KUMAR <123072668+amr2t@users.noreply.github.com>
2023-07-19 18:15:17 +0530

    Register & Login API created.

--- arg_platform/src/main/java/com/trade/arg_platform/controllers/UserController.java
+++ arg_platform/src/main/java/com/trade/arg_platform/controllers/UserController.java
@@ -1,22 +1,28 @@
 package com.trade.arg_platform.controllers;
 
+import com.trade.arg_platform.entity.user;
+import com.trade.arg_platform.services.userservice;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController
 {
 
-//    @Autowired
-//    private userservice userService;
-//
-//    @GetMapping("/user")
-//    public List<user> getUser()
-//    {
-//        return this.userService.getAllUsers();
-//    }
-//
-//    @PostMapping("/register")
-//    public user register(@RequestBody user user) throws Exception {
-//        return userService.saveNewUser(user);
-//    }
+    @Autowired
+    private userservice userService;
+
+    @PostMapping("/register")
+    public user register(@RequestBody user user) throws Exception {
+        return userService.saveNewUser(user);
+    }
+
+    @PostMapping("/login")
+    public user login(@RequestBody user user) throws Exception {
+        return userService.loginUser(user);
+    }
+
+
 }
--- arg_platform/src/main/java/com/trade/arg_platform/services/userservice.java
+++ arg_platform/src/main/java/com/trade/arg_platform/services/userservice.java
@@ -1,24 +1,34 @@
 package com.trade.arg_platform.services;
 
+import com.trade.arg_platform.dao.userRepository;
+import com.trade.arg_platform.entity.user;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class userservice {
 
 
-//    @Autowired
-//    private userRepository repo;
-//    public user saveNewUser(user user) throws Exception{
-//        user tempObj = repo.findByEmail(user.getEmail());
-//        if(tempObj!=null) {
-//            throw new Exception("This user already exists ");
-//        }
-//        if(user.getCart()==null) {
-//            Cart tempCart = new Cart();
-//            Cservice.saveCart(tempCart);
-//            user.setCart(tempCart);
-//        }
-//        return repo.save(user);
-//    }
+    @Autowired
+    private userRepository repo;
+    public user saveNewUser(user user) throws Exception{
+        user tempObj = repo.findByEmail(user.getEmail());
+        if(tempObj!=null) {
+            throw new Exception("This user already exists ");
+        }
+        return repo.save(user);
+    }
 
+    public user loginUser(user user) throws Exception
+    {
+        user tempObj = repo.findByEmail(user.getEmail());
+        if(tempObj==null) {
+            throw new Exception("User does not exists ");
+        }
+
+        if(!tempObj.getPassword().equals(user.getPassword())){
+            throw new Exception("Wrong password");
+        }
+        return tempObj;
+    }
 }
